d50db4c292e2a5c39217728fb4bb7e5c71b7a00f
WagnerSousaLima <wagner.sousalima@gmail.com>
2024-07-06 18:37:49 -0300

    created Controller and  user model

---
+++ src/main/java/com/wslprojects/todoList/user/UserController.java
@@ -0,0 +1,16 @@
+package com.wslprojects.todoList.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+
+        System.out.println(userModel.getName());
+
+    }
+
+}
